d05e06549d0a6a6f91fb32c8935e2799f20b5634
Sameep <sameepsi@gmail.com>
2019-04-05 21:03:34 +0530

    Updated solcover json file

--- .solcover.js
+++ .solcover.js
@@ -4,5 +4,5 @@ module.exports = {
     port: 8555,
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    skipFiles : ["test/EscrowWithoutToken.sol", "test/TestToken.sol", "token/ITokenContract.sol"]
+    skipFiles : ["escrow/EscrowProxy.sol", "test/TestToken.sol", "token/ITokenContract.sol"]
 };
