e1adb56549807979cd196e05a4bfe7e64db106ca
Austin <austin.williams@onewayfunction.com>
2019-04-09 12:11:32 +1000

    fix migration issue

--- migrations/rewards/5_Rewards_Migration.js
+++ migrations/rewards/5_Rewards_Migration.js
@@ -1,8 +1,14 @@
 var OBRewards = artifacts.require("OBRewards");
-var Escrow_v1_0 = artifacts.require("Escrow_v1_0");
+var Escrow = artifacts.require("Escrow");
 var OBToken = artifacts.require("OBToken");
 
 module.exports = function(deployer) {
   //This is dummy data
-  deployer.deploy(OBRewards, "50000000000000000000", 432000, Escrow_v1_0.address, OBToken.address);
+  deployer.deploy(
+    OBRewards,
+    "50000000000000000000",
+    432000,
+    Escrow.address,
+    OBToken.address
+  );
 };
