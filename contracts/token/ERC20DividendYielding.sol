2222127de33cd391ae0217a6531c1ff59bb7892b
Austin <austin.williams@onewayfunction.com>
2019-04-24 14:13:26 -0600

    implement dividend-yielding ERC20 token

---
+++ contracts/token/ERC20DividendYielding.sol
@@ -0,0 +1,349 @@
+/* solium-disable max-len */
+
+pragma solidity ^0.5.4;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+/**
+ * @title Dividend Yielding ERC20 token
+ * @dev Token holders are entitled to a pro rata share of any ETH this contract
+ * receives while they are holding tokens.
+ * This contract is derived from Zeppelin's ERC20Detailed token contract:
+ * https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC20/ERC20Detailed.sol
+ * The `_transfer`, `_mint`, and `_burn` functions have been modified.
+ * Dividend receivers can withdraw thier funds via `claimDividends`.
+ */
+contract ERC20DividendYielding is IERC20 {
+    using SafeMath for uint256;
+
+    struct Dividend {
+        uint256 unclaimedSinceLastAccounting;
+        uint256 contractBalanceAtLastAccounting;
+    }
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowed;
+
+    mapping (address => Dividend) private _dividends;
+
+    uint256 private _totalSupply;
+
+    uint256 private _dividendsReleased;
+
+    string private _name;
+
+    string private _symbol;
+
+    uint8 private _decimals;
+
+
+    constructor (
+      string memory name,
+      string memory symbol,
+      uint8 decimals
+    )
+    public
+    {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+    }
+
+    /**
+     * @dev Fallback accepts ETH.
+     */
+    function () external payable {}
+
+    /**
+     * @return the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @return the symbol of the token.
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @return the number of decimals of the token.
+     */
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev Total number of tokens in existence.
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Total amount of ETH released as dividends by this contract.
+     */
+    function dividendsReleased() public view returns (uint256) {
+        return _dividendsReleased;
+    }
+
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param owner The address to query the balance of.
+     * @return A uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    /**
+     * @dev Gets the amount dividends (ETH) a specified address can claim.
+     * @param account The address to query the dividend balance of.
+     * @return A uint256 representing the amount of dividends owed to the
+     * passed address.
+     */
+    function dividendBalanceOf(address account) public view returns (uint256) {
+        return _dividends[account].unclaimedSinceLastAccounting.add(
+            _earnedSinceLastAccounting(account)
+        );
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to
+     * a given spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for
+     * the spender.
+     */
+    function allowance(address owner, address spender)
+        public
+        view
+        returns (uint256)
+    {
+        return _allowed[owner][spender];
+    }
+
+    /**
+     * @dev Transfer token to a specified address.
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens
+     * on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that
+     * someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not
+     * required as per the specification, and other compliant implementations
+     * may not emit the event.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(address from, address to, uint256 value)
+        public
+        returns (bool)
+    {
+        _transfer(from, to, value);
+        _approve(from, msg.sender, _allowed[from][msg.sender].sub(value));
+        return true;
+    }
+
+    /**
+     * @dev Increase the amount of tokens that an owner allowed to a spender.
+     * approve should be called when _allowed[msg.sender][spender] == 0. To
+     * increment allowed value is better to use this function to avoid 2 calls
+     * (and wait until the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * Emits an Approval event.
+     * @param spender The address which will spend the funds.
+     * @param addedValue The amount of tokens to increase the allowance by.
+     */
+    function increaseAllowance(address spender, uint256 addedValue)
+        public
+        returns (bool)
+    {
+        _approve(
+            msg.sender,
+            spender,
+            _allowed[msg.sender][spender].add(addedValue)
+        );
+        return true;
+    }
+
+    /**
+     * @dev Decrease the amount of tokens that an owner allowed to a spender.
+     * approve should be called when _allowed[msg.sender][spender] == 0. To
+     * decrement allowed value is better to use this function to avoid 2 calls
+     * (and wait until the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * Emits an Approval event.
+     * @param spender The address which will spend the funds.
+     * @param subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseAllowance(
+        address spender,
+        uint256 subtractedValue
+    )
+        public
+        returns (bool)
+    {
+        _approve(msg.sender, spender, _allowed[msg.sender][spender].sub(subtractedValue));
+        return true;
+    }
+
+    /**
+     * @dev Allows dividends to be claimed.
+     * @param account The address that will receive its dividend.
+     */
+    function claimDividends(address payable account) public {
+        //require(msg.sender == account) //optional
+        uint256 total = dividendBalanceOf(account);
+        require(total > 0);
+        _dividends[account].unclaimedSinceLastAccounting = 0;
+        _dividends[account].contractBalanceAtLastAccounting = address(this).balance.sub(total);
+        _dividendsReleased = _dividendsReleased.add(total);
+        account.transfer(total);
+    }
+
+    /**
+     * @dev Gets the amount of ETH the contract has received since the last
+     * dividend accounting for an address.
+     * @param account The address in question.
+     */
+    function _contractProfitSinceLastAccounting(address account)
+        internal
+        view
+        returns (uint256)
+    {
+        return address(this).balance.add(_dividendsReleased).sub(_dividends[account].contractBalanceAtLastAccounting);
+    }
+
+    /**
+     * @dev Gets the amount of ETH an address has earned (as dividends) since
+     * the last dividend accounting for the address.
+     * @param account The address in question.
+     */
+    function _earnedSinceLastAccounting(address account)
+        internal
+        view
+        returns (uint256)
+    {
+        return _contractProfitSinceLastAccounting(account).mul(balanceOf(account)).div(totalSupply());
+    }
+
+    /**
+     * @dev Transfer token for a specified addresses.
+     * Performs dividend accounting before moving tokens.
+     * @param from The address to transfer from.
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+        //dividend accounting for from address
+        _dividends[from].unclaimedSinceLastAccounting = _dividends[from].unclaimedSinceLastAccounting.add(_earnedSinceLastAccounting(from));
+        _dividends[from].contractBalanceAtLastAccounting = address(this).balance;
+
+        //dividend accounting for to address
+        _dividends[to].unclaimedSinceLastAccounting = _dividends[to].unclaimedSinceLastAccounting.add(_earnedSinceLastAccounting(to));
+        _dividends[to].contractBalanceAtLastAccounting = address(this).balance;
+
+        //token balance accounting
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    /**
+     * @dev Internal function that mints an amount of the token and assigns it
+     * to an account. This encapsulates the modification of balances such that
+     * the proper events are emitted.
+     * Perfroms dividend accouting before minting.
+     * @param account The account that will receive the created tokens.
+     * @param value The amount that will be created.
+     */
+    function _mint(address account, uint256 value) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        //dividend accounting
+        _dividends[account].contractBalanceAtLastAccounting = address(this).balance;
+
+        //token accounting
+        _totalSupply = _totalSupply.add(value);
+        _balances[account] = _balances[account].add(value);
+        emit Transfer(address(0), account, value);
+    }
+
+    /**
+     * @dev Internal function that burns an amount of the token of a given
+     * account.
+     * @param account The account whose tokens will be burnt.
+     * @param value The amount that will be burnt.
+     */
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        //dividend accounting
+        _dividends[account].unclaimedSinceLastAccounting = _dividends[account].unclaimedSinceLastAccounting.add(_earnedSinceLastAccounting(account));
+        _dividends[account].contractBalanceAtLastAccounting = address(this).balance;
+
+        //token accounting
+        _totalSupply = _totalSupply.sub(value);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+    }
+
+    /**
+     * @dev Approve an address to spend another addresses' tokens.
+     * @param owner The address that owns the tokens.
+     * @param spender The address that will spend the tokens.
+     * @param value The number of tokens that can be spent.
+     */
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowed[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    /**
+     * @dev Internal function that burns an amount of the token of a given
+     * account, deducting from the sender's allowance for said account. Uses
+     * the internal burn function.
+     * Emits an Approval event (reflecting the reduced allowance).
+     * @param account The account whose tokens will be burnt.
+     * @param value The amount that will be burnt.
+     */
+    function _burnFrom(address account, uint256 value) internal {
+        _burn(account, value);
+        _approve(account, msg.sender, _allowed[account][msg.sender].sub(value));
+    }
+}
